31341e7911c12c6c79a6f1c23548948647b39b01
Sekiel <im.sekiel@gmail.com>
2022-01-30 18:51:34 -0800

    medium1 small_problem complete

--- small_problems/medium1.rb
+++ small_problems/medium1.rb
@@ -1,11 +1,11 @@
-##Problem 1
+## Problem 1
 # def rotate_array(array)
 #   dupl = array.dup
 #   dupl.append(dupl.shift)
 #   dupl
 # end
 
-##Problem 2
+## Problem 2
 # def rotate_rightmost_digits(numbers, index)
 #   array = numbers.digits
 #   elements = array[0, index]
@@ -15,7 +15,7 @@
 
 # end
 
-##Problem 3
+## Problem 3
 
 # def max_rotation(number)
 #   length = number.to_s.length
@@ -45,7 +45,7 @@
 #   number
 # end
 
-##Problem 4
+## Problem 4
 
 # def lights(num)
 #   lights = (1..num).to_a # => [1, 2, 3, 4, 5]
@@ -82,26 +82,124 @@
 
 # end
 
-##Problem 5
-def diamond(num)
-  ast = '*'
-  counter = 1
-  spaces = num
-  return puts '*' if num == 1
-  #Top side
-  loop do
-    space_side = (spaces - counter) / 2
-    puts ' ' * space_side + ast * counter + ' ' * space_side
-    counter += 2
-    break if counter >= num
-  end
+## Problem 5
 
-  #Bottom Side
-  loop do
-    space_side = (spaces - counter) / 2
-    puts ' ' * space_side + ast * counter + ' ' * space_side
-    counter -= 2
-    break if counter <= 0
-  end
+# def diamond(num)
+#   ast = '*'
+#   counter = 1
+#   spaces = num
+#   return puts '*' if num == 1
+#   #Top side
+#   loop do
+#     space_side = (spaces - counter) / 2
+#     puts ' ' * space_side + ast * counter + ' ' * space_side
+#     counter += 2
+#     break if counter >= num
+#   end
 
-end
+#   #Bottom Side
+#   loop do
+#     space_side = (spaces - counter) / 2
+#     puts ' ' * space_side + ast * counter + ' ' * space_side
+#     counter -= 2
+#     break if counter <= 0
+#   end
+
+# end
+
+## Problem 6 - Stack Machine Interpretation
+
+# def minilang(string)
+#   register = 0
+#   stack = []
+
+#   # Turn string into array containing strings and ints
+#   array = string.split.map do |element|
+#             if element.to_i == 0
+#               element
+#             else
+#               element.to_i
+#             end
+#           end
+
+#   array.each do |element|
+#     if element.is_a? Integer
+#       register = element
+#       next
+#     end
+#     case element
+#     when "PUSH"
+#       stack << register
+#     when "ADD"
+#       register = register + stack[-1]
+#       stack.pop
+#     when "SUB"
+#       register = register - stack[-1]
+#       stack.pop
+#     when "MULT"
+#       register = register * stack[-1]
+#       stack.pop
+#     when "DIV"
+#       register = register / stack[-1]
+#       stack.pop
+#     when "MOD"
+#       register = register % stack[-1]
+#       stack.pop
+#     when "POP"
+#       register = stack.pop
+#     when "PRINT"
+#       puts register
+#     end
+#   end
+
+# end
+
+## Problem 7 - Word to Digit
+
+# NUMBERS = { 'one' => '1', 'two' => '2', 'three' => '3', 'four' => '4', 
+#             'five' => '5', 'six' => '6', 'seven' => '7', 'eight' => '8', 
+#             'nine' => '9', 'zero' => '0' }
+
+# def word_to_digit(string)
+#   NUMBERS.keys.each do |word|
+#     string.gsub!(/\b#{word}\b/, NUMBERS[word])
+#   end
+#   string
+# end
+
+## Problem 8 - Fibonacci Numbers (Recursion)
+
+# def fibonacci(n)
+#   return 1 if n <= 2
+#   fibonacci(n - 1) + fibonacci(n - 2)
+# end
+
+# p fibonacci(41)
+
+## Problem 9 - Fibonacci Numbers (Procedural)
+
+# def fibonacci(nth)
+#   return 1 if nth == 1 || nth == 2
+#   nth = nth - 2
+#   fib = [1, 1]
+#   counter = 0
+#   loop do
+#     fib << fib[counter] + fib[counter + 1]
+#     counter += 1
+#     break if counter >= nth
+#   end
+#   fib[-1].to_s[-1]
+
+# end
+
+## Problem 10 - Fibonacci Numbers (Last Digit)
+## My solution was .to_s[-1].to_i, but I couldn't compute bigger numbers
+
+# def fibonacci_last(nth)
+#   start = [1, 1]
+#   3.upto(nth) do
+#     start = [start.last, (start.first + start.last) % 10]
+#   end
+
+#   start
+# end
